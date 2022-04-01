1de7c1d816a704d637b2e98cea90c2ded45c60d1
ranabanik <rana.banik@vanderbilt.edu>
2022-04-01 00:31:57 -0500

    Smoothing continue ...

--- Codes/pca_umap_hbdscan_gmm.py
+++ Codes/pca_umap_hbdscan_gmm.py
@@ -253,48 +253,6 @@ if __name__ != '__main__':
     dend = shc.dendrogram(shc.linkage(data_norm_ss, method='ward'))
     plt.show()
 
-# +------------------------------------------------+
-# |     undecimated discreet wavelet denoising     |
-# +------------------------------------------------+
-if __name__ != '__main__':
-    def madev(d, axis=None):
-        """ Mean absolute deviation of a signal """
-        return np.mean(np.absolute(d - np.mean(d, axis)), axis)
-
-    def wavelet_denoising(x, wavelet='bior4.4', level=2):
-        coeff = pywt.wavedec(x, wavelet, mode="per")
-        sigma = (1/0.6745) * madev(coeff[-level])
-        uthresh = sigma * np.sqrt(2 * np.log(len(x)))
-        coeff[1:] = (pywt.threshold(i, value=uthresh, mode='hard') for i in coeff[1:])
-        return pywt.waverec(coeff, wavelet, mode='per')
-
-    signal = data[nS, :]
-
-    for wav in pywt.wavelist():
-        print(wav)
-        # try:
-        #     filtered = wavelet_denoising(signal, wavelet=wav, level=1)
-        # except:
-        #     pass
-
-    filtered = wavelet_denoising(signal, wavelet='bior4.4')
-    plt.figure(figsize=(10, 6))
-    plt.plot(signal, label='Raw')
-    plt.plot(filtered, label='Filtered')
-    plt.legend()
-    plt.title(f"DWT Denoising with {wav} Wavelet", size=15)
-    plt.show()
-
-    fig, ax = plt.subplots(2, 1, figsize=(20, 8), dpi=200)
-    # plt.subplot(411)
-    ax[0].plot(signal)
-    plt.title("raw")
-    # plt.subplot(412)
-    ax[1].plot(filtered)
-    plt.title("filtered")
-    plt.show()
-    print(np.min(filtered))
-
 # +---------------+
 # |   tree plots  |
 # +---------------+
@@ -644,6 +602,7 @@ def msmlfunc(mspath, regID, threshold, exprun_name=None):
         plt.show()
     return
 
+# posLip = r'/media/banikr2/DATA/MALDI/210427_Chen_pos_lipid' #
 posLip = r'/media/banikr2/DATA/MALDI/fromCardinal/PosLip'
 mspath = glob(os.path.join(posLip, '*.imzML'))[0]
 print(mspath)
@@ -651,30 +610,157 @@ print(mspath)
 # msmlfunc(mspath, regID=1, threshold=0.95, exprun_name='sav_golay_norm')   # todo: change values
 
 imze = IMZMLExtract(mspath)
-spectra0_orig = imze.get_region_array(3, makeNullLine=False)
+spectra0_orig = imze.get_region_array(3, makeNullLine=True)
 
 # +---------------------------+
 # |   demonstrate smoothing   |
 # +---------------------------+
-if __name__ == '__main__':
-    nX = 90 # np.random.randint(spectra0_orig.shape[0])
-    nY = 41 # np.random.randint(spectra0_orig.shape[1])
+if __name__ != '__main__':
+    nX = 90  # np.random.randint(spectra0_orig.shape[0])
+    nY = 41  # np.random.randint(spectra0_orig.shape[1])
     print(nX, nY)
     refSpec = spectra0_orig[nX, nY, :]
     fig, ax = plt.subplots(figsize=(16, 10), dpi=200)
     p = ax.plot(refSpec)
     wl_ = 3  # todo: 5 is better
     po_ = 2
-    smoothSpec_sav = _smooth_spectrum(refSpec, method='savgol', window_length=wl_, polyorder=po_)
-    p, = ax.plot(smoothSpec_sav)
+    outspectrum = _smooth_spectrum(refSpec, method='savgol', window_length=wl_, polyorder=po_)
+    p, = ax.plot(outspectrum)
     plt.subplots_adjust(bottom=0.25)
     ax_slide = plt.axes([0.25, 0.1, 0.65, 0.03])
     win_len = Slider(ax_slide, 'window length', valmin=wl_, valmax=99, valinit=99, valstep=2)
     def update(val):
         current_v = int(win_len.val)
-        smoothSpec_sav = _smooth_spectrum(refSpec, method='savgol', window_length=current_v, polyorder=po_)
-        p.set_ydata(smoothSpec_sav)
+        outspectrum = _smooth_spectrum(refSpec, method='savgol', window_length=current_v, polyorder=po_)
+        p.set_ydata(outspectrum)
         fig.canvas.draw()
     win_len.on_changed(update)
     plt.show()
 
+# +------------------------------------------------+
+# |     undecimated discreet wavelet denoising     |
+# +------------------------------------------------+
+if __name__ != '__main__':
+    def madev(d, axis=None):
+        """ Mean absolute deviation of a signal """
+        return np.mean(np.absolute(d - np.mean(d, axis)), axis)
+
+    def wavelet_denoising(x, wavelet='bior4.4', level=2):
+        coeff = pywt.wavedec(x, wavelet, mode="per")
+        sigma = (1/0.6745) * madev(coeff[-level])
+        uthresh = sigma * np.sqrt(2 * np.log(len(x)))
+        coeff[1:] = (pywt.threshold(i, value=uthresh, mode='hard') for i in coeff[1:])
+        return pywt.waverec(coeff, wavelet, mode='per')
+
+    # signal = data[nS, :]
+
+    for wav in pywt.wavelist():
+        print(wav)
+        # try:
+        #     filtered = wavelet_denoising(signal, wavelet=wav, level=1)
+        # except:
+        #     pass
+
+    filtered = wavelet_denoising(signal, wavelet='bior4.4')
+    plt.figure(figsize=(10, 6))
+    plt.plot(signal, label='Raw')
+    plt.plot(filtered, label='Filtered')
+    plt.legend()
+    plt.title(f"DWT Denoising with {wav} Wavelet", size=15)
+    plt.show()
+
+    fig, ax = plt.subplots(2, 1, figsize=(20, 8), dpi=200)
+    # plt.subplot(411)
+    ax[0].plot(signal)
+    plt.title("raw")
+    # plt.subplot(412)
+    ax[1].plot(filtered)
+    plt.title("filtered")
+    plt.show()
+    print(np.min(filtered))
+
+# +---------------------------+
+# |      ms_peak_picker       |
+# +---------------------------+
+if __name__ != '__main__':
+    import ms_peak_picker
+
+    # ms_peak_picker.peak_picker(spectra0_orig)
+    picking_method = "quadratic"
+    mz_array = np.array([23, 34])
+    intensity_array = np.array([230, 340])
+    peak_list = ms_peak_picker.pick_peaks(mz_array, intensity_array, fit_type=picking_method)
+
+##
+    nX = 90  # np.random.randint(spectra0_orig.shape[0])
+    nY = 41  # np.random.randint(spectra0_orig.shape[1])
+    print(nX, nY)
+    refSpec = spectra0_orig[nX, nY, :][200:250]
+    # smoothing signal before finding peaks
+    wl_ = 5
+    po_ = 2
+    outspectrum = _smooth_spectrum(refSpec, method='savgol', window_length=wl_, polyorder=po_)
+    peaks, properties = signal.find_peaks(outspectrum, prominence=1e6)
+    print(peaks.shape, properties)
+    # plt.plot(refSpec)
+    plt.plot(outspectrum)
+    plt.plot(peaks, outspectrum[peaks], "x")
+    # plt.vlines(x=peaks, ymin=outspectrum[peaks] - properties["prominences"],
+    #            ymax=outspectrum[peaks], color="C1")
+    # plt.hlines(y=properties["width_heights"], xmin=properties["left_ips"],
+    #            xmax=properties["right_ips"], color="C1")
+    plt.show()
+
+class MaldiTofSpectrum(np.ndarray):
+    """Numpy NDArray subclass representing a MALDI-TOF Spectrum."""
+    def __new__(cls, peaks):
+        """Create a MaldiTofSpectrum.
+        Args:
+            peaks: 2d array or list of tuples or list of list containing pairs
+                of mass/charge to intensity.
+        Raises:
+            ValueError: If the input data is not in the correct format.
+        """
+        peaks = np.asarray(peaks).view(cls)
+        if peaks.ndim != 2 or peaks.shape[1] != 2:
+            raise ValueError(
+                f'Input shape of {peaks.shape} does not match expected shape '
+                'for spectrum [n_peaks, 2].'
+            )
+        return peaks
+
+    @property
+    def n_peaks(self):
+        """Get number of peaks of the spectrum."""
+        return self.shape[0]
+
+    @property
+    def intensities(self):
+        """Get the intensities of the spectrum."""
+        return self[:, 1]
+
+    @property
+    def mass_to_charge_ratios(self):
+        """Get mass-t0-charge ratios of spectrum."""
+        return self[:, 0]
+
+MOCK_DATA = [
+    MaldiTofSpectrum(
+        [[0.0,   5.0],
+         [10.7,  8.0],
+         [150.4, 10.],
+         [1000,  3.0]
+         ]
+    ),  # Mean intensity 6.5
+    MaldiTofSpectrum(
+        [[0.0,   15.0],
+         [10.7,  0.0],
+         [150.4, 10.],
+         [1000,  3.0]
+         ]
+    ),  # Mean intensity 7 or 9.3333 (with ignore zero intensity)
+]
+print(MOCK_DATA[0].shape, MOCK_DATA[0].n_peaks, MOCK_DATA[0].mass_to_charge_ratios)
+
+# MD = MaldiTofSpectrum(MOCK_DATA)
+# print(MD)

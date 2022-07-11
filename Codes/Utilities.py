00dce8478a45f95c6ce01999d95c2d9c6b670481
Rana Banik <rana.banik@vanderbilt.edu>
2022-07-11 13:33:11 -0500

    umam on resamp + msalign not required

--- Codes/Utilities.py
+++ Codes/Utilities.py
@@ -526,6 +526,136 @@ class ImzmlAll(object):
             im.callbacks.connect('changed', update)
         plt.show()
 
+def umap_it(data, n_neighbors=12, n_components=3,
+            min_dist=0.025, plot_umap=True, random_state=20210131):
+    """
+    from grid-parameter search in Hang Hu paper(visual plot)
+    from grid-param Hang Hu paper(visual plot)
+    :param data: standardized data to be umapped
+    :param n_neighbors:
+    :param n_components:
+    :param min_dist:
+    :param random_state:
+    :return:
+    """
+    reducer = UMAP(n_neighbors=n_neighbors,
+                   # default 15, The size of local neighborhood (in terms of number of neighboring sample points) used for manifold approximation.
+                   n_components=n_components,  # default 2, The dimension of the space to embed into.
+                   metric='cosine',
+                   # default 'euclidean', The metric to use to compute distances in high dimensional space.
+                   n_epochs=1000,
+                   # default None, The number of training epochs to be used in optimizing the low dimensional embedding. Larger values result in more accurate embeddings.
+                   learning_rate=1.0,  # default 1.0, The initial learning rate for the embedding optimization.
+                   init='spectral',
+                   # default 'spectral', How to initialize the low dimensional embedding. Options are: {'spectral', 'random', A numpy array of initial embedding positions}.
+                   min_dist=min_dist,  # default 0.1, The effective minimum distance between embedded points.
+                   spread=1.0,
+                   # default 1.0, The effective scale of embedded points. In combination with ``min_dist`` this determines how clustered/clumped the embedded points are.
+                   low_memory=False,
+                   # default False, For some datasets the nearest neighbor computation can consume a lot of memory. If you find that UMAP is failing due to memory constraints consider setting this option to True.
+                   set_op_mix_ratio=1.0,
+                   # default 1.0, The value of this parameter should be between 0.0 and 1.0; a value of 1.0 will use a pure fuzzy union, while 0.0 will use a pure fuzzy intersection.
+                   local_connectivity=1,
+                   # default 1, The local connectivity required -- i.e. the number of nearest neighbors that should be assumed to be connected at a local level.
+                   repulsion_strength=1.0,
+                   # default 1.0, Weighting applied to negative samples in low dimensional embedding optimization.
+                   negative_sample_rate=5,
+                   # default 5, Increasing this value will result in greater repulsive force being applied, greater optimization cost, but slightly more accuracy.
+                   transform_queue_size=4.0,
+                   # default 4.0, Larger values will result in slower performance but more accurate nearest neighbor evaluation.
+                   a=None,
+                   # default None, More specific parameters controlling the embedding. If None these values are set automatically as determined by ``min_dist`` and ``spread``.
+                   b=None,
+                   # default None, More specific parameters controlling the embedding. If None these values are set automatically as determined by ``min_dist`` and ``spread``.
+                   random_state=random_state,
+                   # default: None, If int, random_state is the seed used by the random number generator;
+                   metric_kwds=None,
+                   # default None) Arguments to pass on to the metric, such as the ``p`` value for Minkowski distance.
+                   angular_rp_forest=False,
+                   # default False, Whether to use an angular random projection forest to initialise the approximate nearest neighbor search.
+                   target_n_neighbors=-1,
+                   # default -1, The number of nearest neighbors to use to construct the target simplcial set. If set to -1 use the ``n_neighbors`` value.
+                   # target_metric='categorical', # default 'categorical', The metric used to measure distance for a target array is using supervised dimension reduction. By default this is 'categorical' which will measure distance in terms of whether categories match or are different.
+                   # target_metric_kwds=None, # dict, default None, Keyword argument to pass to the target metric when performing supervised dimension reduction. If None then no arguments are passed on.
+                   # target_weight=0.5, # default 0.5, weighting factor between data topology and target topology.
+                   transform_seed=42,
+                   # default 42, Random seed used for the stochastic aspects of the transform operation.
+                   verbose=True,  # default False, Controls verbosity of logging.
+                   unique=False
+                   # default False, Controls if the rows of your data should be uniqued before being embedded.
+                   )
+    data_umap = reducer.fit_transform(data)
+    # +---------------+
+    # |   UMAP plot   |
+    # +---------------+
+    if plot_umap:
+        plt.figure(figsize=(12, 10))
+        plt.scatter(data_umap[:, 0], data_umap[:, 1], facecolors='None', edgecolors=cm.tab20(10), alpha=0.5)
+        plt.xlabel('UMAP1', fontsize=30)  # only difference part from last one
+        plt.ylabel('UMAP2', fontsize=30)
+
+        # theme
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.tick_params(size=10, color='black')
+
+        ax = plt.gca()  # Get the current Axes instance
+
+        for axis in ['top', 'bottom', 'left', 'right']:
+            ax.spines[axis].set_linewidth(3)
+        ax.tick_params(width=3)
+        plt.show()
+    return data_umap
+
+def hdbscan_it(data, HDBSCAN_soft = False, min_cluster_size = 250,
+               min_samples = 30, cluster_selection_method = 'eom'):
+    """
+    :param data:
+    :param HDBSCAN_soft:
+    :param min_cluster_size:
+    :param min_samples:
+    :param cluster_selection_method:
+    :return:
+    """
+    if HDBSCAN_soft:
+        clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples,
+                                    cluster_selection_method=cluster_selection_method, prediction_data=True) \
+            .fit(data)
+        soft_clusters = hdbscan.all_points_membership_vectors(clusterer)
+        labels = np.argmax(soft_clusters, axis=1)
+    else:
+        clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples,
+                                    cluster_selection_method=cluster_selection_method).fit(data)
+        labels = clusterer.labels_
+
+    # process HDBSCAN data
+    n_clusters_est = np.max(labels) + 1
+    if HDBSCAN_soft:
+        title = 'estimated number of clusters: ' + str(n_clusters_est)
+    else:
+        labels[labels == -1] = 19
+        title = 'estimated number of clusters: ' + str(n_clusters_est) + ', noise pixels are coded in cyan'
+    # plot
+    plt.figure(figsize=(12, 10))
+    plt.scatter(data[:, 0], data[:, 1], facecolors='None', edgecolors=cm.tab20(labels), alpha=0.9)
+    plt.xlabel('UMAP1', fontsize=30)  # only difference part from last one
+    plt.ylabel('UMAP2', fontsize=30)
+
+    # theme
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.tick_params(size=10, color='black')
+    plt.title(title, fontsize=20)
+
+    ax = plt.gca()  # Get the current Axes instance
+
+    for axis in ['top', 'bottom', 'left', 'right']:
+        ax.spines[axis].set_linewidth(3)
+    ax.tick_params(width=3)
+    plt.show()
+    chart(data, labels)
+    return labels
+
 class MaldiTofSpectrum(np.ndarray):
     """Numpy NDArray subclass representing a MALDI-TOF Spectrum."""
     def __new__(cls, peaks):
@@ -3078,123 +3208,11 @@ def msmlfunc6(mspath, regID, threshold=0.85, save_rseg=False): # exprun,
     # +------------------+
     # |      UMAP        |
     # +------------------+
-    u_neigh = 12  # from grid-parameter search in Hang Hu paper(visual plot)
-    u_comp = 3
-    u_min_dist = 0.025  # from grid-param Hang Hu paper(visual plot)
-    reducer = UMAP(n_neighbors=u_neigh,
-                   # default 15, The size of local neighborhood (in terms of number of neighboring sample points) used for manifold approximation.
-                   n_components=u_comp,  # default 2, The dimension of the space to embed into.
-                   metric='cosine',
-                   # default 'euclidean', The metric to use to compute distances in high dimensional space.
-                   n_epochs=1000,
-                   # default None, The number of training epochs to be used in optimizing the low dimensional embedding. Larger values result in more accurate embeddings.
-                   learning_rate=1.0,  # default 1.0, The initial learning rate for the embedding optimization.
-                   init='spectral',
-                   # default 'spectral', How to initialize the low dimensional embedding. Options are: {'spectral', 'random', A numpy array of initial embedding positions}.
-                   min_dist=u_min_dist,  # default 0.1, The effective minimum distance between embedded points.
-                   spread=1.0,
-                   # default 1.0, The effective scale of embedded points. In combination with ``min_dist`` this determines how clustered/clumped the embedded points are.
-                   low_memory=False,
-                   # default False, For some datasets the nearest neighbor computation can consume a lot of memory. If you find that UMAP is failing due to memory constraints consider setting this option to True.
-                   set_op_mix_ratio=1.0,
-                   # default 1.0, The value of this parameter should be between 0.0 and 1.0; a value of 1.0 will use a pure fuzzy union, while 0.0 will use a pure fuzzy intersection.
-                   local_connectivity=1,
-                   # default 1, The local connectivity required -- i.e. the number of nearest neighbors that should be assumed to be connected at a local level.
-                   repulsion_strength=1.0,
-                   # default 1.0, Weighting applied to negative samples in low dimensional embedding optimization.
-                   negative_sample_rate=5,
-                   # default 5, Increasing this value will result in greater repulsive force being applied, greater optimization cost, but slightly more accuracy.
-                   transform_queue_size=4.0,
-                   # default 4.0, Larger values will result in slower performance but more accurate nearest neighbor evaluation.
-                   a=None,
-                   # default None, More specific parameters controlling the embedding. If None these values are set automatically as determined by ``min_dist`` and ``spread``.
-                   b=None,
-                   # default None, More specific parameters controlling the embedding. If None these values are set automatically as determined by ``min_dist`` and ``spread``.
-                   random_state=RandomState,
-                   # default: None, If int, random_state is the seed used by the random number generator;
-                   metric_kwds=None,
-                   # default None) Arguments to pass on to the metric, such as the ``p`` value for Minkowski distance.
-                   angular_rp_forest=False,
-                   # default False, Whether to use an angular random projection forest to initialise the approximate nearest neighbor search.
-                   target_n_neighbors=-1,
-                   # default -1, The number of nearest neighbors to use to construct the target simplcial set. If set to -1 use the ``n_neighbors`` value.
-                   # target_metric='categorical', # default 'categorical', The metric used to measure distance for a target array is using supervised dimension reduction. By default this is 'categorical' which will measure distance in terms of whether categories match or are different.
-                   # target_metric_kwds=None, # dict, default None, Keyword argument to pass to the target metric when performing supervised dimension reduction. If None then no arguments are passed on.
-                   # target_weight=0.5, # default 0.5, weighting factor between data topology and target topology.
-                   transform_seed=42,
-                   # default 42, Random seed used for the stochastic aspects of the transform operation.
-                   verbose=True,  # default False, Controls verbosity of logging.
-                   unique=False
-                   # default False, Controls if the rows of your data should be uniqued before being embedded.
-                   )
-    data_umap = reducer.fit_transform(images_feat_flat_norm_ss)
-    # +---------------+
-    # |   UMAP plot   |
-    # +---------------+
-    if plot_umap:
-        plt.figure(figsize=(12, 10))
-        plt.scatter(data_umap[:, 0], data_umap[:, 1], facecolors='None', edgecolors=cm.tab20(10), alpha=0.5)
-        plt.xlabel('UMAP1', fontsize=30)  # only difference part from last one
-        plt.ylabel('UMAP2', fontsize=30)
-
-        # theme
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        plt.tick_params(size=10, color='black')
-
-        ax = plt.gca()  # Get the current Axes instance
-
-        for axis in ['top', 'bottom', 'left', 'right']:
-            ax.spines[axis].set_linewidth(3)
-        ax.tick_params(width=3)
-        plt.show()
-
+    data_umap = umap_it(images_feat_flat_norm_ss)
     # +-------------+
     # |   HDBSCAN   |
     # +-------------+
-    HDBSCAN_soft = False
-    min_cluster_size = 250
-    min_samples = 30
-    cluster_selection_method = 'eom'  # eom
-    if HDBSCAN_soft:
-        clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples,
-                                    cluster_selection_method=cluster_selection_method, prediction_data=True) \
-            .fit(data_umap)
-        soft_clusters = hdbscan.all_points_membership_vectors(clusterer)
-        labels = np.argmax(soft_clusters, axis=1)
-    else:
-        clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples,
-                                    cluster_selection_method=cluster_selection_method).fit(data_umap)
-        labels = clusterer.labels_
-
-    # process HDBSCAN data
-    n_clusters_est = np.max(labels) + 1
-    if HDBSCAN_soft:
-        title = 'estimated number of clusters: ' + str(n_clusters_est)
-    else:
-        labels[labels == -1] = 19
-        title = 'estimated number of clusters: ' + str(n_clusters_est) + ', noise pixels are coded in cyan'
-    # plot
-    plt.figure(figsize=(12, 10))
-    plt.scatter(data_umap[:, 0], data_umap[:, 1], facecolors='None', edgecolors=cm.tab20(labels), alpha=0.9)
-    plt.xlabel('UMAP1', fontsize=30)  # only difference part from last one
-    plt.ylabel('UMAP2', fontsize=30)
-
-    # theme
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)
-    plt.tick_params(size=10, color='black')
-    plt.title(title, fontsize=20)
-
-    ax = plt.gca()  # Get the current Axes instance
-
-    for axis in ['top', 'bottom', 'left', 'right']:
-        ax.spines[axis].set_linewidth(3)
-    ax.tick_params(width=3)
-    plt.show()
-    chart(data_umap, labels)
-    # plt.suptitle("n_neighbors={}, Cosine metric".format(u_neigh))
-
+    labels = hdbscan_it(data_umap)
     # +------------------------------+
     # |  cluster peak M/Zs with KDE  |
     # +------------------------------+
@@ -3216,7 +3234,6 @@ def msmlfunc6(mspath, regID, threshold=0.85, save_rseg=False): # exprun,
             (peakmzs_feat >= s[mi_][mdx]) & (peakmzs_feat < s[mi_][mdx + 1]))]
         # print(len(cluster), "\n", cluster)
         clusters.append(cluster)
-    #
 
     plt.plot(s, e, 'orange')
     plt.vlines(cluster_center, ymin=min(e), ymax=max(e), colors='blue')
